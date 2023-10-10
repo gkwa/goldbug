c0896d143532fd67da818ebc716568af2345bf33
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-10 16:53:31 -0700

    Switch to SetDefaultLoggerJson and SetDefaultLoggerText

--- goldbug.go
+++ goldbug.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-func SetDefaultLogger() {
+func SetDefaultLoggerText() {
 	logLevel := &slog.LevelVar{} // INFO
 	logLevel.Set(slog.LevelDebug)
 	opts := slog.HandlerOptions{
@@ -19,6 +19,24 @@ func SetDefaultLogger() {
 		},
 	}
 	handler1 := slog.NewTextHandler(os.Stderr, &opts)
+
+	slog.SetDefault(slog.New(handler1))
+}
+
+func SetDefaultLoggerJson(level slog.Level) {
+	logLevel := &slog.LevelVar{} // INFO
+	logLevel.Set(level)
+	opts := slog.HandlerOptions{
+		AddSource: true,
+		Level:     logLevel,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey && len(groups) == 0 {
+				return slog.Attr{}
+			}
+			return a
+		},
+	}
+	handler1 := slog.NewJSONHandler(os.Stderr, &opts)
 
 	slog.SetDefault(slog.New(handler1))
 }
